5de18079cf750e1febd8c797dde97f7e60a7d8b9
Williams Calado <williamscalado@gmail.com>
2022-03-05 20:08:47 -0300

    login update

--- src/Controllers/Login/index.ts
+++ src/Controllers/Login/index.ts
@@ -9,20 +9,20 @@ dotenv.config()
 
 
 export const loginUser = async (req: Request, res: Response) => {
-    
+
     try {
         const data: Ilogin = req.body
-        await loginModel.validate(data)        
+        await loginModel.validate(data)
         const loginUserExec = await loginUserUseCase(data)
-        if(!loginUserExec) {res.status(401); throw new Error()}
+        if (!loginUserExec) { res.status(401); throw new Error() }
 
         const { status } = loginUserExec;
         res.status(status).json(loginUserExec)
-        
+
     } catch (error) {
         res.status(401).json(error)
-        
+
     }
-   
+
 
 }
